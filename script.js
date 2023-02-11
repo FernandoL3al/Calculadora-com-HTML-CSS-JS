4597ecb67b2da2ebff025ca38240637455983356
FernandoL3al <luisfernandopkbrasil@gmail.com>
2023-02-11 02:48:35 -0300

    um pouco de flex+grid adicionado
    e uma brincadeirazinha com DOM

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-let x = get
-let y
+function equal(){
+    const tela = document.getElementById('screen')
+    const numer = document.createElement('p')
+
+    numer.innerText = "Minha pika grosaa"
+    tela.appendChild(numer)
+}
 
